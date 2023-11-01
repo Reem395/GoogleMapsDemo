452a3d791199b1078d63e48e20362127529be695
Reem Khalil <76693917+Reem395@users.noreply.github.com>
2023-11-01 17:40:47 +0200

    env removed

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:sizer/sizer.dart';
 
 import 'controller/autoComplete cubit/google_maps_cubit.dart';
@@ -11,12 +9,6 @@ import 'firebase_options.dart';
 import 'view/home.dart';
 
 void main() async {
-  // await dotenv.load(fileName: "lib/.env" ); //path to your .env file);
-  //   print("API Key from env :${dotenv.env['GOOGLE_API_KEY']}");
-  // const platform = MethodChannel('api_channel');
-  // final googleApiKey = dotenv.env['GOOGLE_API_KEY'];
-  // await platform.invokeMethod('setApiKey', googleApiKey);
-  
   WidgetsFlutterBinding.ensureInitialized();
   Bloc.observer = MyBlocObserver();
   await Firebase.initializeApp(
@@ -28,7 +20,6 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
